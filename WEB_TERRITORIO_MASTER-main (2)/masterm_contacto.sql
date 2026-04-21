1976ddcee7863de2590337721a2466cb010e9e7f
Andrey Garcia Garcia <158564801+AndreyGarciaGarcia@users.noreply.github.com>
2026-04-21 12:56:34 +0200

    Add files via upload

---
+++ WEB_TERRITORIO_MASTER-main (2)/masterm_contacto.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE masterm_contacto;
+USE masterm_contacto;
+
+CREATE TABLE contactos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_completo VARCHAR(100) NOT NULL,
+    fecha_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
