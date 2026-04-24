fe1b7399127f2794bfa002e9ed7a96ec9d3b2877
VitalyRuso <vitalijsolovev837@gmail.com>
2026-04-24 16:22:14 +0200

    Delete WEB_TERRITORIO_MASTER-main directory

--- WEB_TERRITORIO_MASTER-main/masterm_contacto.sql
+++
@@ -1,9 +0,0 @@
--- masterm_contacto.sql
-CREATE DATABASE IF NOT EXISTS masterm_contacto;
-USE masterm_contacto;
-
-CREATE TABLE IF NOT EXISTS contactos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_completo VARCHAR(100) NOT NULL,
-    fecha_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
