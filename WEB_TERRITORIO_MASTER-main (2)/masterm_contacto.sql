e2b120f07d03cf04777e397162d834f8e7bd053c
Andrey Garcia Garcia <158564801+AndreyGarciaGarcia@users.noreply.github.com>
2026-04-24 12:41:36 +0200

    Delete WEB_TERRITORIO_MASTER-main (2) directory

--- WEB_TERRITORIO_MASTER-main (2)/masterm_contacto.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE masterm_contacto;
-USE masterm_contacto;
-
-CREATE TABLE contactos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_completo VARCHAR(100) NOT NULL,
-    fecha_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
